aa85c15297d96e8f935e327cb6e907bfc25a67ca
Rahul Warule <rahulwarule@Rahuls-MacBook-Pro.local>
2021-08-19 11:22:04 +0530

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -22,20 +22,24 @@ class MyApp extends StatelessWidget {
 }
 
 class SplashScreen extends StatefulWidget {
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
-    Future.delayed(Duration(seconds: 3), (){
-      Navigator.pushAndRemoveUntil(context, PageTransition(duration: Duration(milliseconds: 300), type: PageTransitionType.leftToRight, child: MovieListScreen()), ModalRoute.withName(""));
+    //add delay for 3 seconds and navigate to MovieList screen
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          PageTransition(
+              duration: Duration(milliseconds: 300),
+              type: PageTransitionType.leftToRight,
+              child: MovieListScreen()),
+          ModalRoute.withName(""));
     });
   }
 
@@ -48,38 +52,49 @@ class _SplashScreenState extends State<SplashScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              //App name and Progressbar
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-
                     Container(
                       child: Text(
                         'IMDB-NX',
-                        style: TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.bold, fontSize: 50.0),
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 50.0),
                         overflow: TextOverflow.clip,
                         textAlign: TextAlign.center,
                       ),
                     ),
-
-                    SpinKitThreeBounce(color: Colors.black, size: 30.0,),
+                    SpinKitThreeBounce(
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
                   ],
                 ),
               ),
 
+              //bottom text
               Align(
                 alignment: Alignment.bottomCenter,
-                child: Text('Designed & Developed By - Rahul Warule',
-                  style: TextStyle(fontFamily: 'Poppins', fontSize: 14.0, color: Colors.black),
+                child: Text(
+                  'Designed & Developed By - Rahul Warule',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14.0,
+                      color: Colors.black),
                   overflow: TextOverflow.clip,
                   textAlign: TextAlign.center,
                 ),
               ),
-              SizedBox(height: 10.0,)
+              SizedBox(
+                height: 10.0,
+              )
             ],
-          )
-      ),
+          )),
     );
   }
 }
